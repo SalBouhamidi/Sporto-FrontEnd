189452263d0c9ee9c2061cc0ddd35267cc97027b
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-11-29 17:06:29 +0100

    SPORTO-5 - SPORTO-4 the api of login and register was consumed successfully

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
 import { DefaultLayout } from '../Pages/Layouts/DefaultLayout'
 import Login from '../Pages/Login'
 import Register from '../Pages/Register'
@@ -6,14 +6,18 @@ import Register from '../Pages/Register'
 
 export const router = createBrowserRouter([
     {
-        element: <DefaultLayout/>,
+        element: <DefaultLayout />,
         children: [
-            {
-                path: "register",
-                element: <Register/>
-            }
+
         ],
+
+    },{
         path: "/login",
-        element: <Login/>
+        element: <Login />
+    },{
+
+        path: "register",
+        element: <Register />
+
     }
 ])
